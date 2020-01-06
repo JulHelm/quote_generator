1c2b5173ee71fc7501382bcd3b05362974c86dbc
JulHelm <julhelm@posteo.de>
2020-01-06 15:42:21 +0100

    finished

--- inc/functions.php
+++ inc/functions.php
@@ -1,34 +1,64 @@
 <?php  
 
-// PHP - Random Quote Generator
-
-// Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
-$quotes = [
-    $quote1 = [
-        "quote" => "", 
-        "source" => "", 
+// An array with all quotes is generated: (with tag category)
+$quotes = [];
+$quotes[] = [
+        "quote" => "Life is a journey, not a destination.", 
+        "source" => "Ralph Waldo Emerson", 
+        "citation" => "...from the internet!", 
+        "year" => "1322",
+        "tag" => "wisom"];
+$quotes[] = [
+        "quote" => "It is not length of life, but depth of life", 
+        "source" => "Ralph Waldo Emerson", 
+        "citation" => "...from the internet!", 
+        "year" => "1755",
+        "tag" => "truth"];
+$quotes[] = [
+        "quote" => "Don't part with your illusions. When they are gone, you may still exist, but you have ceased to live.", 
+        "source" => "Mark Twain", 
+        "citation" => "...from the internet!", 
+        "year" => "1890",];
+$quotes[] = [
+        "quote" => "It’s not about ideas. It’s about making ideas happen.", 
+        "source" => "Scott Belsky", 
+        "citation" => "fluentu.com", 
+        "year" => "",];
+$quotes[] = [
+        "quote" => "Always deliver more than expected.", 
+        "source" => "Larry Page", 
         "citation" => "", 
-        "year" => "",],
-    $quote2 = [
-        "quote" => "", 
-        "source" => "", 
-        "citation" => "", 
-        "year" => "",],        
-    $quote3 = [
-        "quote" => "", 
-        "source" => "", 
-        "citation" => "", 
-        "year" => "",],
-    ]
+        "year" => "",];
 
+$bg_colors = ["#36b55c", "red", "blue"];
 
-// Create the getRandomQuuote function and name it getRandomQuote
+// Function that randomly pickes a quote out of the array given and return it
+function getRandomQuote($array){
+    $random_number = rand(0, 4);
+    return $array[$random_number];
+}
 
+// Function that prints the picked quote according to the quotes structure
+function printQuote($array){
+    $quote_object = getRandomQuote($array);
+    $result = '
+        <p class="quote">' . $quote_object["quote"] . '</p> 
+        <p class="source">' . $quote_object["source"] . '</p>'; 
+    if ($quote_object["citation"]) {
+        $result = $result . '<span class="citation">' . $quote_object["citation"] . '</span>';
+    }
+    if ($quote_object["year"]) {
+        $result = $result . '<span class="year">' . $quote_object["year"] . '</span>';
+    }
+    // added optional tag element to output if set
+    if ($quote_object["tag"]) {
+        $result = $result . '<span class="tag">' . $quote_object["tag"] . '</span>';
+    }
+    echo $result;
+}
 
-
-// Create the printQuote funtion and name it printQuote
-
-
-
-?>
+// Funtion to generate a random bg color from the array $bg_colors:
+function new_bg_color($bg_colors){
+    $random_number = rand(0, 2);
+    return $bg_colors[$random_number];
+}
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+include "inc/functions.php";
+
+// getting the background color from the function new_bg_color
+$bg = new_bg_color($bg_colors);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +13,16 @@
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
+  <!-- Auto refresh all 20 seconds: -->
+  <meta http-equiv="refresh" content="2">
 </head>
-<body>
+<!-- inserting the random background color: -->
+<body style="background: <?php echo $bg; ?>">
   <div class="container">
     <div id="quote-box">
-      <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
-    </div>
+      <!-- printQuote function is called to display what is randomly picked out of the quotes array -->
+      <?php printQuote($quotes) ?> 
+    </div> 
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
 </body>
